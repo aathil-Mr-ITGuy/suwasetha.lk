f568a4981ef9aecff31192ecc1192f423d2297f1
lakshimaprabhani <lakshimaprabhani@gmail.com>
2019-11-14 12:40:13 +0530

    bookappointments add

---
+++ src/app/bookappointment/bookappointment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookappointment',
+  templateUrl: './bookappointment.component.html',
+  styleUrls: ['./bookappointment.component.css']
+})
+export class BookappointmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
